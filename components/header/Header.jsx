d5e00ae5fefd05034bc17dd08ab5be71fe65455e
Roovin <roovin.bansal@cleardigital.com>
2024-04-20 13:47:16 +0530

    update header and tailwind

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="header">
+        <div className="container">
+          <div className="mainHeader">
+            <div className="logoWrap">
+              <Image src={'/logo/logo.png'} alt='Logo' width={334} height={140} />
+            </div>
+            <div className="main-nav">
+              <div className="nav">
+                <ul>
+                  <li>
+                    <Link href="#">About Us</Link>
+                    {/* <div className="subMenu">
+                      <ul>
+                        <li>
+                          <Link></Link>
+                        </li>
+                      </ul>
+                    </div> */}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
---
+++ components/header/headerData.js
@@ -0,0 +1,35 @@
+export const headerData = [
+    {
+        id:"1",
+        nav: "About Us",
+        subMenu: [
+            {
+                subNav: "Our Mission",
+            },
+            {
+                subNav: "Our Team",
+            },
+        ]
+    },
+    {
+        id:"2",
+        nav: "Our Work",
+    },
+    {
+        id:"3",
+        nav: "Our Blog",
+    },
+    {
+        id:"4",
+        nav: "Our News",
+    },
+    {
+        id:"5",
+        nav: "Events",
+    },
+    {
+        id:"6",
+        nav: "Contact Us",
+    },
+
+]
