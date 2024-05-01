622d6c7e8bbe2c7e64bf1ebd410e25442e3e0cfd
Bhawana <bhawana@webenertia.com>
2024-05-01 16:51:10 +0530

    update OurMission & faq page-data

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React from "react";
 
-export default function Faq({data}) {
+export default function Faq({ data }) {
   return (
     <section className="pat">
-        <div className="container">
-            <div className="intro text-center mb-[40px]">
-                <h2 className='text-blue h1'>{data.title}</h2>
-            </div>
-            {
-                data?.patData.map((item, i) => {
-                    return (
-                        <div key={i} className="item mb-[40px]">
-                           <div className="titleWrap">
-                            <h6 className='text-blue leading-[0.5]' ><span className='inline-block pr-[10px] mb-[20px] relative'>{i+1}.</span>{item.title}</h6>
-                            <p className='pl-[20px]'>{item.description}</p>
-                           </div>
-                        </div>
-                    )
-                })
-            }
+      <div className="container">
+        <div className="intro text-center mb-[40px]">
+          <h2 className="text-blue h1">{data.title}</h2>
         </div>
+        {data?.patData.map((item, i) => {
+          return (
+            <div key={i} className="item mb-[40px]">
+              <div className="titleWrap">
+                <h6 className="text-blue leading-[0.5]">
+                  <span className="inline-block pr-[10px] mb-[20px] relative">
+                    {i + 1}.
+                  </span>
+                  {item.title}
+                </h6>
+                <p
+                  className={`pl-[20px] ${item.description2 ? "mb-5" : ""}`}
+                  dangerouslySetInnerHTML={{ __html: item.description }}
+                />
+                {item.description2 && (
+                  <p
+                    className="pl-[20px]"
+                    dangerouslySetInnerHTML={{ __html: item.description2 }}
+                  />
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
-  )
+  );
 }
--- public/data/patData.js
+++ public/data/patData.js
@@ -1,31 +1,32 @@
-export const pat = 
+export const pat =
+{
+  title: "FAQ: Psychedelic-Assisted Therapy",
+  patData: [
     {
-        title: "FAQ: Psychedelic-Assisted Therapy",
-        patData: [
-            {
-                id: "1",
-                title: "What is psychedelic-assisted therapy (PAT)? ",
-                description: "Psychedelic-assisted therapy (PAT) is an emerging field that explores the therapeutic potential of psychedelic substances, such as ketamine, psilocybin, MDMA, LSD, and ayahuasca, in combination with psychotherapy. These substances have a long history of traditional use in various cultures for spiritual and healing purposes. In recent decades, there has been a resurgence of interest in harnessing their therapeutic effects for mental health treatment. The approach involves administering a controlled dose of the psychedelic substance in a therapeutic setting, guided by trained professionals to facilitate profound and transformative experiences. This is accompanied by multiple sessions with the therapist before, during, and after administration."
-            },
-            {
-                id: "2",
-                title: "What does current research on PAT show?  ",
-                description: "Scientific research on psychedelic-assisted therapy has shown promising results, particularly in the treatment of mental health conditions like Major Depressive Disorder (MDD), anxiety, Post-Traumatic Stress Disorder (PTSD), and addiction. In patients who have failed conventional therapies, clinical trials have demonstrated the potential of psychedelics to induce states of heightened introspection and increased neuroplasticity, leading to breakthroughs in therapy. Studies have shown that these substances can create a unique opportunity for individuals to confront and process deep-seated traumas, facilitating long-lasting positive changes in their psychological well-being. "
-            },
-            {
-                id: "3",
-                title: "What is the therapeutic use of ketamine and other psychedelics?",
-                description: "Ketamine is a dissociative medicine with psychedelic properties. Ketamine is approved for the treatment of PTSD and MDD in the US and Ukraine. Combining ketamine with cognitive behavioral therapy has shown considerable success in treating severe depression.[1] This process is referred to as ketamine-assisted psychotherapy (KAP) and is the focus of HUT’s novel group KAP model in Ukraine. </br> Additionally, MDMA-assisted therapy has gained attention for its success in treating severe PTSD, with clinical trials demonstrating significant reductions in symptoms and improvements in patients' quality of life. Psilocybin has shown efficacy in alleviating symptoms of treatment-resistant depression. Research conducted at institutions such as Johns Hopkins University and Imperial College London highlighted the positive impact of psilocybin-assisted therapy on mood, emotional regulation, and overall mental health."
-            },
-            {
-                id: "4",
-                title: "Where is PAT approved? ",
-                description: "Currently, the US Food & Drug Administration (FDA) is evaluating the first application for US regulatory approval of PAT using MDMA.[2] Listed as a Breakthrough Therapy by the FDA in 2017 for severe PTSD and MDD, the FDA’s decision is expected this year. Other applications for PAT are expected to follow in the US, UK, and Europe, and Australia approved psilocybin and MDMA assisted therapies for severe PTSD and treatment-resistant depression in 2023.[3]"
-            },
-            {
-                id: "5",
-                title: "What does the future hold for PAT in mental health treatment?",
-                description: "Though the treatments are undergoing further analysis, the results thus far suggest that psychedelic-assisted therapy holds promise as a transformative and effective intervention for serious mental health disorders. Continued research is essential to better understand the mechanisms of action, optimize treatment protocols, and ensure the safe implementation of these therapies. The field has sparked growing interest from mental health professionals, researchers, and policymakers, prompting a reevaluation of societal attitudes towards psychedelic substances and their potential role in modern therapeutic practices. As the evidence continues to accumulate, psychedelic-assisted therapy may offer new hope for those who have not found relief through conventional approaches. "
-            },
-        ]
-    }
+      id: "1",
+      title: "What is psychedelic-assisted therapy (PAT)? ",
+      description: "Psychedelic-assisted therapy (PAT) is an emerging field that explores the therapeutic potential of psychedelic substances, such as ketamine, psilocybin, MDMA, LSD, and ayahuasca, in combination with psychotherapy. These substances have a long history of traditional use in various cultures for spiritual and healing purposes. In recent decades, there has been a resurgence of interest in harnessing their therapeutic effects for mental health treatment. The approach involves administering a controlled dose of the psychedelic substance in a therapeutic setting, guided by trained professionals to facilitate profound and transformative experiences. This is accompanied by multiple sessions with the therapist before, during, and after administration."
+    },
+    {
+      id: "2",
+      title: "What does current research on PAT show?  ",
+      description: "Scientific research on psychedelic-assisted therapy has shown promising results, particularly in the treatment of mental health conditions like Major Depressive Disorder (MDD), anxiety, Post-Traumatic Stress Disorder (PTSD), and addiction. In patients who have failed conventional therapies, clinical trials have demonstrated the potential of psychedelics to induce states of heightened introspection and increased neuroplasticity, leading to breakthroughs in therapy. Studies have shown that these substances can create a unique opportunity for individuals to confront and process deep-seated traumas, facilitating long-lasting positive changes in their psychological well-being. "
+    },
+    {
+      id: "3",
+      title: "What is the therapeutic use of ketamine and other psychedelics?",
+      description: "Ketamine is a dissociative medicine with psychedelic properties. Ketamine is approved for the treatment of PTSD and MDD in the US and Ukraine. Combining ketamine with cognitive behavioral therapy has shown considerable success in treating severe depression.[1] This process is referred to as ketamine-assisted psychotherapy (KAP) and is the focus of HUT’s novel group KAP model in Ukraine. ",
+      description2: "Additionally, MDMA-assisted therapy has gained attention for its success in treating severe PTSD, with clinical trials demonstrating significant reductions in symptoms and improvements in patients' quality of life. Psilocybin has shown efficacy in alleviating symptoms of treatment-resistant depression. Research conducted at institutions such as Johns Hopkins University and Imperial College London highlighted the positive impact of psilocybin-assisted therapy on mood, emotional regulation, and overall mental health.  ",
+    },
+    {
+      id: "4",
+      title: "Where is PAT approved? ",
+      description: "Currently, the US Food & Drug Administration (FDA) is evaluating the first application for US regulatory approval of PAT using MDMA.[2] Listed as a Breakthrough Therapy by the FDA in 2017 for severe PTSD and MDD, the FDA’s decision is expected this year. Other applications for PAT are expected to follow in the US, UK, and Europe, and Australia approved psilocybin and MDMA assisted therapies for severe PTSD and treatment-resistant depression in 2023.[3]"
+    },
+    {
+      id: "5",
+      title: "What does the future hold for PAT in mental health treatment?",
+      description: "Though the treatments are undergoing further analysis, the results thus far suggest that psychedelic-assisted therapy holds promise as a transformative and effective intervention for serious mental health disorders. Continued research is essential to better understand the mechanisms of action, optimize treatment protocols, and ensure the safe implementation of these therapies. The field has sparked growing interest from mental health professionals, researchers, and policymakers, prompting a reevaluation of societal attitudes towards psychedelic substances and their potential role in modern therapeutic practices. As the evidence continues to accumulate, psychedelic-assisted therapy may offer new hope for those who have not found relief through conventional approaches. "
+    },
+  ]
+}
