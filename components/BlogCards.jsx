9d05615fbce8b2e0886953aa967f8bb0a2a6367c
Roovin <roovinbansal@gmail.com>
2024-05-03 08:09:35 +0530

    update blog cards

--- components/BlogCards.jsx
+++ components/BlogCards.jsx
@@ -10,14 +10,14 @@ export default function BlogCards({ data }) {
         <div className="intro text-center mb-[20px]">
           <h2 className="text-blue">{data?.title}</h2>
         </div>
-        <div className="cardWrapper flex md:flex-wrap">
+        <div className="cardWrapper flex lg:flex-wrap">
           {data?.cards.map((item, i) => {
             var title = item?.title;
             var titleWithUrl = title.split(" ").join("-");
             return (
               <div
                 key={i}
-                className="card group w-[calc(33.33%-30px)] mx-[15px] relative overflow-hidden border-[2px] border-black rounded-[8px] phablet:w-[calc(50%-20px)] phablet:mx-[10px] sm:w-full sm:mx-0 md:mb-[20px] "
+                className="card group w-[calc(33.33%-30px)] mx-[15px] relative overflow-hidden border-[2px] border-black rounded-[8px] tablet:w-[calc(50%-20px)] tablet:mx-[10px] tablet:mb-[20px] phablet:w-[calc(50%-20px)] phablet:mx-[10px] sm:w-full sm:mx-0 md:mb-[20px] "
               >
                 <Link
                   href={item?.btnUrl + titleWithUrl}
@@ -25,7 +25,7 @@ export default function BlogCards({ data }) {
                 >
                   .
                 </Link>
-                <div className="imgWrap h-[275px] overflow-hidden ipad:h-[200px] tablet:h-[150px]">
+                <div className="imgWrap h-[275px] overflow-hidden ipad:h-[200px]">
                   <Image
                     src={item?.imgUrl}
                     alt={item?.alt}
